db3ee947e05d2cdec9992b456d9787c86a165478
MrtnOmwenga <martin36449@gmail.com>
2022-12-27 08:06:54 -0800

    Debugging

--- main.c
+++ main.c
@@ -48,6 +48,7 @@ int main(int argc, char *argv[])
 	  if (i == 2)
 	    {
 	      fprintf(stderr, "l%d: unknown instruction %s\n", line_number, token);
+	      exit(EXIT_FAILURE);
 	    }
 	  if (strcmp(token, instructions[i].opcode) == 0)
 	    {
